ce6c7fc00fd3c8ee5d231fe0a594d66032f1d199
Troy Miller <troydasheroreo@gmail.com>
2015-03-03 17:15:48 -0500

    save

--- adventure.py
+++ adventure.py
@@ -1,17 +1,31 @@
 """
 Library of stuff to make a text adventure
 """
+
+import diceroll as roll 
+
 class Entity():
     
     def __init__(self):
         self.health = 100
+        self.speed = 1
+        self.hunger = 0
+        self.attack = roll.roll(3,6)
+        self.defense = roll.roll(3,6)
+
+    def __str__(self):
+       f = "Health: {}\nSpeed: {}\nHunger: {}\nAttack: {}\nDefense: {}"
+       return f.format(self.hunger,self.speed,self.attack,self.health,self.defense)
+         
 
 
 class NPC(Entity):
     pass
 
 class Player(Entity):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.health = 120
 
 class Monster(NPC):
     pass
@@ -21,4 +35,9 @@ class Villager(NPC):
 
 if __name__ == '__main__':
     player = Player()
-    print(player.health)
+    print ("-~-" * 10)
+    print("Player ",player)
+
+    npc = NPC()
+    print ("-~-" * 10)
+    print("NPC",npc)
---
+++ diceroll.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Dice module
+"""
+import random
+def roll(n, sides):
+    n = int(n)
+    sides = int(sides)
+    total = 0
+    for count in range(n):
+        total += random.randint(1,sides)
+    return total
+if __name__ == '__main__':
+    for count in range(20):
+        print(roll(3,6))
+    print()
