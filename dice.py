5375bfcaec05466b492a1e0346d75a2896d96f32
Troy Miller <troydasheroreo@gmail.com>
2015-05-18 16:46:08 -0400

    save

--- dice.py
+++ dice.py
@@ -2,20 +2,26 @@
 A simple dice rolling module
 """
 import random as r
+import colors as c
 
 def rolldie(sides=6):
     return r.randint(1,sides)
-def roll(times,sides):
+def roll(amount=1,sides=6):
     total = 0
-    for count in range(times):
+    for count in range(amount):
         total += rolldie(sides)
     return total
-        
+def parse_roll(text):
+    """Parses traditional dice notation (ex:3d6)"""
+    amount, sides = text.split('d')
+    return roll(int(amount), int(sides))
 if __name__ == "__main__":
-    number = rolldie(6)
-    print(number)
+   for count in range(20):
+        #number = rolldie()
+        #print(intnumber,end=" ")
+    print(c.random_color(), end=' ')   
+    print(parse_roll('2d20'), end=' ')
+    
 
-    strength = roll(3,6)
-    print(strength)
 
     
